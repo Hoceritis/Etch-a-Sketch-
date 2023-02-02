119db9b7b3925ce583cf4c0fa3e64cdccc67ff94
Sullivan <adressecours0.1@gmail.com>
2023-02-02 18:18:40 +0100

    Event listener added for hover on squares - wip still need to be modified

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let mainDiv = document.createElement('div')
 mainDiv.setAttribute('id','main-div')
 document.body.append(mainDiv)
 
+let row = document.createElement('div')
+let square = document.createElement('div')
+
 function grid(){
     makeBlock(4)
 }
@@ -9,15 +12,24 @@ function grid(){
 function makeBlock(boxNumber){
     for(let i = 0; i < boxNumber ; i++){
         let row = document.createElement('div')
-        row.className = 'row'
         mainDiv.append(row)
+        row.className = 'row'
         row.style.backgroundColor = 'rgba(255, 255, 128, .5)';
         for (let j = 0; j < boxNumber; j++){
             let square = document.createElement('div')
-            square.className = 'box'
             row.append(square)
+            square.className = 'box'
         }
     }
 }
 
 grid()
+
+document.querySelectorAll('.box').forEach(item => {
+    item.addEventListener('mouseover', event => {
+        let hover = event.target
+        hover.classList.toggle('.boxTwo')
+        //hover.style.backgroundColor = 'black'
+        console.log(hover)
+    })
+  })
