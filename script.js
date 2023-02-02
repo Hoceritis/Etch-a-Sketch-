46b3b766e27690d9e2020e99d2103a373f84cbe7
Sullivan <adressecours0.1@gmail.com>
2023-02-02 15:23:36 +0100

    grid function created + css added to visualise the grid

---
+++ script.js
@@ -0,0 +1,23 @@
+let mainDiv = document.createElement('div')
+mainDiv.setAttribute('id','main-div')
+document.body.append(mainDiv)
+
+function grid(){
+    makeBlock(4)
+}
+
+function makeBlock(boxNumber){
+    for(let i = 0; i < boxNumber ; i++){
+        let row = document.createElement('div')
+        mainDiv.append(row)
+        row.style.backgroundColor = 'red'
+        for (let j = 0; j < boxNumber; j++){
+            let square = document.createElement('div')
+            square.className = 'box'
+            square.style.backgroundColor ='green'
+            row.append(square)
+        }
+    }
+}
+
+grid()
