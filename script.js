148a48ea96d6a86f536e2422dcb78644996be652
Sullivan <adressecours0.1@gmail.com>
2023-02-07 17:39:36 +0100

    added a new div for the grid and a flex display - wip

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ let prompt;
 
 let mainDiv = document.createElement('div')
 mainDiv.setAttribute('id','main-div')
-document.body.append(mainDiv)
+
+// grid's div
+
+let gridDiv = document.createElement('div')
+gridDiv.setAttribute('id','grid-div')
 
 // squares and rows
 
@@ -17,15 +21,17 @@ let square = document.createElement('div')
 
 let button = document.createElement('button')
 button.setAttribute('id','button')
-mainDiv.append(button)
 button.textContent =' Click me'
+
+document.body.append(button, mainDiv)
+mainDiv.append(gridDiv)
 
 // functions
 
 function grid(boxNumber){
     for(let i = 0; i < boxNumber ; i++){
         let row = document.createElement('div')
-        mainDiv.append(row)
+        gridDiv.append(row)
         row.className = 'row'
         row.style.backgroundColor = 'rgba(255, 255, 128, .5)';
         for (let j = 0; j < boxNumber; j++){
@@ -54,8 +60,15 @@ button.addEventListener('click', () => {
   function newGrid(integer){
     let convertToNumber = parseInt(integer, 10)
     if(Number.isInteger(convertToNumber) && convertToNumber < 100){
-        grid(convertToNumber)
+        if(grid(convertToNumber) && convertToNumber > 30)
+        { 
+            document.getElementsByClassName('box').style.width = "20px"
+            document.getElementsByClassName('box').style.height = "20px"
+        }
         mouseOver()
     } else
         console.log("It's not a valid number")
   }
+
+  // reduce the size of the box when a treshold is reached
+  // delete the previous grid to create a new one
